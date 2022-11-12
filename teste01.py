ec509edb10fb7e3ee6ebf1cd52fa4366a4a725e1
Mutneja <118028587+leumiranda@users.noreply.github.com>
2022-11-12 12:13:19 -0300

    Variaveis
    TESTE 1

---
+++ teste01.py
@@ -0,0 +1,25 @@
+print('Hello, World!')
+
+# VARIAVEIS E CONSTANTES.
+# NO MEU PEQUENO CONCEITO DE VARIÁVEL, É UM ESPAÇO NA MEMÓRIA E ELE PODE RECEBER UM DETERMINADO VALOR.
+
+
+# Strings 
+nome = 'Leandro'
+apelido = "Leandro"
+
+
+# Numeros inteiros.
+idade = 27
+
+# Numeros Reais ou flutuantes.
+peso = 84.350
+bit = 0.00000000001
+
+# Boleanos
+Ensolarado = False
+Chuvendo = True
+
+
+# Nullo ou vazio?
+futuro = None
